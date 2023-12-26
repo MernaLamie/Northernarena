e97dce5b2ab7559698395d70af9778069ed4a4d3
MernaLamie <merna.lamie20@gmail.com>
2023-12-26 02:25:59 +0200

    UpdateContactus

---
+++ NorthArena/Migrations/20231226002452_CreateNew.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NorthArena.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNew : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "ContactUs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "ContactUs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "ContactUs");
+
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "ContactUs");
+        }
+    }
+}
--- NorthArena/Models/ContactUs.cs
+++ NorthArena/Models/ContactUs.cs
@@ -8,5 +8,10 @@
         public string Email { get; set; }
         public string Phone { get; set; }
 
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+
+
     }
 }
